db736496a65ee27c74d0af459d2318bd2460fd6a
rmukkamala <tony@Rohans-MacBook-Pro.local>
2022-03-23 22:08:20 -0700

    Adding fixes for deployment files

--- modules/location_microservice/app/udaconnect/models.py
+++ modules/location_microservice/app/udaconnect/models.py
@@ -12,6 +12,14 @@ from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
+class Person(db.Model):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
+
 
 
 class Location(db.Model):
